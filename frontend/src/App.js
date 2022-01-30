12886442aca0fd94bc6095b203ac5e0247de5c06
Lucas Pauker <lucas.pauker@gmail.com>
2022-01-30 12:59:30 -0800

    Add frontend/backend barebones

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
+  return (
+    <div id="App">
+      <p>The current time is {currentTime}.</p>
+    </div>
+  );
+}
+
+export default App;
